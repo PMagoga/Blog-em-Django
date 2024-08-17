770effdb81c6d7f99f14b35d25121dfac1fb5dd8
PMagoga <pmagoga@gmail.com>
2024-08-17 20:00:57 -0300

    adicionando os models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog.models import Category, Comment, Post
+
+
+class CategoyAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Category, CategoyAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
